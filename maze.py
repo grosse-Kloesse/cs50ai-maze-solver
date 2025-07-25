4e751a17178b13d95bfb3a4b3679e0c4274b67ec
Bin Lin <lin@ktc.mw.tu-dresden.de>
2025-07-25 23:07:50 +0200

    v0.1 includ Maze Solver

---
+++ maze.py
@@ -0,0 +1,63 @@
+from search import bfs
+
+class Maze:
+    def __init__(self,filename):
+        with open(filename) as f:
+            contents = f.read()
+    
+        lines = [line.rstrip("\r\n") for line in contents.splitlines()]
+
+        self.height = len(lines)
+        self.width = max(len(line) for line in lines)
+        self.maze = []
+
+        self.start = None
+        self.goal = None
+
+        for i, line in enumerate(lines):
+            row = []
+            for j in range(self.width):
+                if j>=len(line):
+                    char=" "
+                else:
+                    char = line[j]
+
+                if char == "A":
+                    self.start = (i,j)
+                    row.append(" ")
+                elif char == "B":
+                    self.goal = (i,j)
+                    row.append(" ")
+                else:
+                    row.append(char)
+            self.maze.append(row)
+
+    def print_maze(self):
+        for i in range(self.height):
+            for j in range(self.width):
+
+                if (i,j) == self.start:
+                    print("A", end = "")
+                elif (i,j) == self.goal:
+                    print("B" , end = "")
+                elif hasattr(self, 'solution') and (i,j) in self.solution:
+                    print ("*",end="") # use * to show path
+                else:
+                    print(self.maze[i][j],end="")
+
+            print()
+
+    def solve(self):
+        def is_wall(i,j):
+            return self.maze[i][j] == "#"
+        
+        path = bfs(self.maze,self.start,self.goal,is_wall)
+        if path is None:
+            print("No Path found!")
+        else:
+            self.solution = set(path)
+
+if __name__ == "__main__":
+    m = Maze("maze.txt")
+    m.solve()
+    m.print_maze()
---
+++ search.py
@@ -0,0 +1,56 @@
+from collections import deque
+
+def bfs(maze, start, goal, is_wall):
+    """
+    Breadth_First Search to find the shortest path in the maze.
+
+    Parameters:
+        maze: 2D list of characters
+        start: (row, col) tuple
+        goal: (row, col) tuple
+        is_wall: function(i, j) -> bool
+
+    Returns:
+        path: list of (row, col) tuples from start to goal
+            or None if no path found    
+    """
+
+    frontier = deque()
+    frontier.append(start)
+
+    came_from = {start:None}
+    visited = set()
+
+    while frontier:
+        current = frontier.popleft()
+
+        if current == goal:
+            # return patn
+            path = []
+            while current is not None:
+                path.append(current)
+                current = came_from[current]
+            path.reverse()
+            return path
+        
+        visited.add(current)
+
+        for neighbor in get_neighbors(current, maze, is_wall):
+            if neighbor not in visited and neighbor not in frontier:
+                came_from[neighbor] = current
+                frontier.append(neighbor)
+
+    return None  # no path
+
+
+def get_neighbors(position, maze, is_wall):
+    i, j = position
+    neighbors = []
+
+    for di, dj in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+        ni, nj = i + di, j + dj
+        if 0 <= ni < len(maze) and 0 <= nj < len(maze[0]):
+            if not is_wall(ni, nj):
+                neighbors.append((ni, nj))
+
+    return neighbors
